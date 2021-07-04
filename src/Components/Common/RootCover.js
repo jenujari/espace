a4be3e22b23c3901433fb5647490c61e638d532a
Jenish Jariwala <jhon.5456@gmail.com>
2021-07-05 00:28:08 +0530

    Coming Soon

---
+++ src/Components/Common/RootCover.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './../../styles/index.scss';
+
+export default function RootCover({ children }) {
+  return (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  )
+}
